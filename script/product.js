98993ab1e4d96c18f218b7da27563115cad5691a
Shakeel <shakeeldvd23@gmail.com>
2024-06-13 09:52:59 +0200

    Modified and implemented table format

--- script/product.js
+++ script/product.js
@@ -1,48 +1,44 @@
 // year
-document.querySelector('[year]').textContent =
-    new Date().getUTCFullYear()
+document.querySelector('[year]').textContent = new Date().getUTCFullYear();
 
-    let container = document.querySelector('[ourStore]')
-    let searchProduct = document.querySelector('[searchProduct]')
-    let highSort = document.querySelector('[highSort]')
-    let lowSort = document.querySelector('[lowSort]')
-    let spinner = document.querySelector('[spinner]')
+let container = document.querySelector('[ourStore]');
+let searchProduct = document.querySelector('[searchProduct]');
+let highSort = document.querySelector('[highSort]');
+let lowSort = document.querySelector('[lowSort]');
+let spinner = document.querySelector('[spinner]');
 
-    let products = JSON.parse(
-        localStorage.getItem('products')
-    )
-    // items/products 
-    let checkoutItems = JSON.parse(localStorage.getItem('checkout'))
-        ? JSON.parse(localStorage.getItem('checkout'))
-        : []
-    
-    function displayProducts(args) {
-        container.innerHTML = ""
-        try {
-            args.forEach(product => {
-                container.innerHTML += `
-                    <div class="card">
-                        <img src="${product.img_url}" class="card-img-top" alt="${product.productName}" loading='lazy'>
-                        <div class="card-body">
-                            <h5 class="card-title">${product.productName}</h5>
-                            <p class="card-text">${product.category}</p>
-                            <p class="card-text">
-                                <span class="text-success fw-bold">Amount</span>
-                                R${product.amount}
-                            </p>
-                            <button type='button' class="btn btn-success" onclick='addToCart(${JSON.stringify(product)})'>Add to cart</button>
-                        </div>
+let products = JSON.parse(localStorage.getItem('products'));
+
+// items/products 
+let checkoutItems = JSON.parse(localStorage.getItem('checkout')) || [];
+
+function displayProducts(args) {
+    container.innerHTML = "";
+    try {
+        args.forEach(product => {
+            container.innerHTML += `
+                <div class="card">
+                    <img src="${product.img_url}" class="card-img-top" alt="${product.productName}" loading='lazy'>
+                    <div class="card-body">
+                        <h5 class="card-title">${product.productName}</h5>
+                        <p class="card-text">${product.category}</p>
+                        <p class="card-text">
+                            <span class="text-success fw-bold">Amount</span>
+                            R${product.amount}
+                        </p>
+                        <button type='button' class="btn btn-success" onclick='addToCart(${JSON.stringify(product)})'>Add to cart</button>
                     </div>
-                `
-            })
-    
-        } catch (e) {
-            container.textContent = "Please try again later"
-        }
+                </div>
+            `;
+        });
+
+    } catch (e) {
+        container.textContent = "Please try again later";
     }
-    displayProducts(products)
-    
-    // keyup
+}
+displayProducts(products);
+
+// keyup
 searchProduct.addEventListener('keyup', () => {
     try {
         if (searchProduct.value.length < 1) {
@@ -60,7 +56,6 @@ searchProduct.addEventListener('keyup', () => {
         container.textContent = e.message || 'Please try again later';
     }
 });
-
 
 // Sorting by amount
 let isToggle = false;
@@ -87,35 +82,37 @@ lowSort.addEventListener('click', () => {
     }
 });
 
-// // add to cart counter
+// add to cart with quantity check
 function addToCart(product) {
-        checkoutItems.push(product);
+    
+        const existingProductIndex = checkoutItems.findIndex(item => item.id === product.id);
+        if (existingProductIndex !== -1) {
+            checkoutItems[existingProductIndex].quantity++;
+        } else {
+            product.quantity = 1;
+            checkoutItems.push(product);
+        }
+
         localStorage.setItem('checkout', JSON.stringify(checkoutItems));
-        
         updateCounter(checkoutItems.length);
 }
 
-// function updateCounter(value) {
-//     localStorage.setItem('cartCounter', value); 
-//     document.querySelector('[counter]').textContent = value || 0; 
-// }
-
-
+function updateCounter(value) {
+    localStorage.setItem('cartCounter', value); 
+    document.querySelector('[counter]').textContent = value || 0; 
+}
 
 function deleteAllData() {
     spinner.classList.remove('d-none');
     setTimeout(() => {
-       
         localStorage.clear();
         products = [];
         checkoutItems = [];
-        
         displayProducts(products);
-        
         spinner.classList.add('d-none');
     }, 2000); 
 }
 
-    window.onload = () => {
-        document.querySelector('[counter]').textContent = checkoutItems.length || 0
-    }
+window.onload = () => {
+    document.querySelector('[counter]').textContent = checkoutItems.length || 0;
+}
